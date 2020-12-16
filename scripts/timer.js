38403baeb7daff44ce37665d793ebfb36022ebd6
vieczorkamil <vieczorkamil@gmial.com>
2020-12-16 18:33:29 +0100

    Dodano index.html css oraz js

---
+++ scripts/timer.js
@@ -0,0 +1,16 @@
+function time() {
+    var dzisiaj = new Date();
+
+    var godzina = dzisiaj.getHours();
+    if (godzina < 10) godzina = "0" + godzina;
+
+    var minuta = dzisiaj.getMinutes();
+    if (minuta < 10) minuta = "0" + minuta;
+
+    var sekunda = dzisiaj.getSeconds();
+    if (sekunda < 10) sekunda = "0" + sekunda;
+
+    document.getElementById("timer").innerHTML = godzina + ":" + minuta + ":" + sekunda;
+
+    setTimeout("time()", 1000);
+}
